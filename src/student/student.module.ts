7379a719ffc1aaf985878b69b827b4c77b7f0a84
memowii <memogl25@gmail.com>
2020-10-04 13:26:37 -0500

    Challenge: Defining the Student Entity

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentService } from './student.service';
+import { Student } from './student.type';
 
 @Module({
-  providers: [StudentService]
+  imports: [TypeOrmModule.forFeature([Student])],
+  providers: [StudentService],
 })
 export class StudentModule {}
